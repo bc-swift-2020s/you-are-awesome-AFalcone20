9287f05dfbd9c74804359f7a8da9034c1155bc23
Alexander Falcone <falconal@bc.edu>
2020-01-21 12:57:23 -0500

    2.8

--- Week One/ViewController.swift
+++ Week One/ViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
            
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,18 +20,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sayItPressed(_ sender: UIButton) {
-       let message1 = "You Are Awesome!"
-       let message2 = "You Are Great!"
-       let message3 = "You Are Amazing!"
         
-        if messageLabel.text == message1 {
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
-        } else {
-            messageLabel.text = message1
+        print(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10{
+            imageNumber = 0
         }
         
+        
+        
+        //       let message1 = "You Are Awesome!"
+        //       let message2 = "You Are Great!"
+        //       let message3 = "You Are Amazing!"
+        //
+        //        if messageLabel.text == message1 {
+        //            messageLabel.text = message2
+        //        } else if messageLabel.text == message2 {
+        //            messageLabel.text = message3
+        //        } else {
+        //            messageLabel.text = message1
+        //        }
     }
     
    
